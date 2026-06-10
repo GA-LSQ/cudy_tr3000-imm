84d119af5c154bbdfe768bab243c0e3a3b1bf401
GA-LSQ <70734094+GA-LSQ@users.noreply.github.com>
2026-06-10 21:25:47 +0800

    Update OpenWrt-part1.sh

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -42,6 +42,15 @@ function git_sparse_clone() {
 #src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main\
 #src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+
+#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+#sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+
 # turboacc
 curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
