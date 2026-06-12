bb6f5b5ae02d93dcd28cb5fc9be9b2c310f713e3
GA-LSQ <70734094+GA-LSQ@users.noreply.github.com>
2026-06-12 14:38:46 +0800

    更新 OpenWrt-part2.sh

--- OpenWrt-part2.sh
+++ OpenWrt-part2.sh
@@ -36,7 +36,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 sed -i 's/192.168.0.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
-sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
+#sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # 修改设备名称
 sed -i 's/immortalwrt/Cudy/g' package/base-files/files/bin/config_generate
