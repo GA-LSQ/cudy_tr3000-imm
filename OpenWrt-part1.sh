1decee7d330e8b4081a58b7af74bc9b842ab4c20
GA-LSQ <70734094+GA-LSQ@users.noreply.github.com>
2026-06-19 20:36:44 +0800

    Update OpenWrt-part1.sh

--- OpenWrt-part1.sh
+++ OpenWrt-part1.sh
@@ -20,6 +20,18 @@
 # sudo update-alternatives --config gcc
 # gcc --version
 
+
+# openclash
+mkdir -p package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+cd ../../../
+
+
 # 自定义函数
 addFeeds(){
   if [ $# == 2 ];then
